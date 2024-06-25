ecdb98138eaf9c166ef1f4620e71db6202e28c74
kanagaraj2k24 <kanagarajr379@gmail.com>
2024-06-25 16:43:07 +0530

    Add files via upload
    i completed the java program of count the length of the string

---
+++ linkedlist.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class linkedlist {
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    String n = sc.nextLine();
+
+        System.out.println(n.length());
+    }
+}
